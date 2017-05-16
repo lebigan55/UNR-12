b03ea4309f364debbab636cd5039beae2f347638
lebigan55 <lebigan55@gmail.com>
2017-05-16 16:09:44 +0800

    cpmit to master
    finish version

---
+++ js/common.js
@@ -0,0 +1,43 @@
+$(function() {
+
+	$(document).ready(function() {
+		$('.zoom-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			closeOnContentClick: false,
+			closeBtnInside: false,
+			mainClass: 'mfp-with-zoom mfp-img-mobile',
+			image: {
+				verticalFit: true,
+				titleSrc: function(item) {
+					return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+				}
+			},
+			gallery: {
+				enabled: true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300, // don't foget to change the duration also in CSS
+				opener: function(element) {
+					return element.find('img');
+				}
+			}
+			
+		});
+	});
+
+	var burger = document.querySelector('.burger-container'),
+	        header = document.querySelector('.header');
+	    
+	    burger.onclick = function() {
+	        header.classList.toggle('menu-opened');
+	    
+	};
+
+	$('#main-form').validate ();
+	$('#main-form1').validate ();
+	
+});
+
+	
